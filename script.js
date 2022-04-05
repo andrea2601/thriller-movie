a1255c784c64a57d84b820acf2bb89ecb8199442
andrea2601 <andrealogiudice26@gmail.com>
2022-04-05 12:22:21 +0200

    MyList added

--- script.js
+++ script.js
@@ -29,14 +29,44 @@ const createCard = (title, imgUrl, popularity, id, date) => {
     const cardWrapper = document.querySelector(".cardWrapper");
     cardWrapper.appendChild(divEl);
 };
+const createMyList = (title, imgUrl, popularity, id, date) => {
 
+    const divEl = document.createElement("div");
+    const h3El = document.createElement("h3");
+    const imgEl = document.createElement("img");
+    const pPopEl = document.createElement("p");
+    const pDateEl = document.createElement("p");
+
+    divEl.classList.add("card");
+    pPopEl.classList.add("popularity");
+    pDateEl.classList.add("date");
+
+    imgEl.setAttribute("src", `https://image.tmdb.org/t/p/original/${imgUrl}`);
+    imgEl.setAttribute("alt", "img poster");
+    divEl.setAttribute("id", id);
+
+    h3El.textContent = title;
+    pPopEl.textContent = popularity;
+    pDateEl.textContent = date;
+
+    divEl.append(imgEl, h3El, pPopEl, pDateEl);
+
+    const cardWrapperList = document.querySelector(".cardWrapperList");
+    cardWrapperList.appendChild(divEl);
+};
+
+const pushEL = (el) => { myList.push(el) };
+
+const myList = [];
 const main = document.querySelector("main");
 
 const apiKey = "652bd852a54702ac6a9aab4afa9bc98a";
 const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=53`;
 
 getMovies(url).then(data => {
-    data.map(movie => {
+    const movieArray = [...data];
+    console.log(movieArray);
+    movieArray.map(movie => {
         createCard(
             movie.title,
             movie.backdrop_path,
@@ -45,5 +75,25 @@ getMovies(url).then(data => {
             movie.release_date
         );
     })
-    console.log(data.results);
+    cards = document.querySelectorAll(".card");
+    console.log(cards);
+    cards.forEach(element => {
+        element.addEventListener("click", () => {
+            myList.push(movieArray.find(item =>
+                item.id == element.id
+            ));
+            console.log(myList);
+            const cardWrapperList = document.querySelector(".cardWrapperList");
+            cardWrapperList.textContent = "";
+            myList.map(movie => {
+                createMyList(
+                    movie.title,
+                    movie.backdrop_path,
+                    movie.popularity,
+                    movie.id,
+                    movie.release_date
+                );
+            })
+        })
+    });
 });
