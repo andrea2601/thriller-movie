c1137407481941e4ff9760c98fb8e849127df730
andrea2601 <andrealogiudice26@gmail.com>
2022-04-05 18:56:35 +0200

    login added

--- script.js
+++ script.js
@@ -11,20 +11,31 @@ const createCard = (title, imgUrl, popularity, id, date) => {
     const imgEl = document.createElement("img");
     const pPopEl = document.createElement("p");
     const pDateEl = document.createElement("p");
+    const plusEl = document.createElement("span");
+    const starEL = document.createElement("span");
 
     divEl.classList.add("card");
     pPopEl.classList.add("popularity");
     pDateEl.classList.add("date");
+    plusEl.classList.add("material-icons");
+    plusEl.classList.add("plus");
+    starEL.classList.add("material-icons");
+    starEL.classList.add("star");
 
     imgEl.setAttribute("src", `https://image.tmdb.org/t/p/original/${imgUrl}`);
     imgEl.setAttribute("alt", "img poster");
     divEl.setAttribute("id", id);
+    plusEl.setAttribute("id", id);
+    starEL.setAttribute("id", id);
 
     h3El.textContent = title;
     pPopEl.textContent = popularity;
     pDateEl.textContent = date;
+    plusEl.textContent = "add";
+    starEL.textContent = "star";
 
-    divEl.append(imgEl, h3El, pPopEl, pDateEl);
+
+    divEl.append(h3El, imgEl, plusEl, starEL, pPopEl, pDateEl);
 
     const cardWrapper = document.querySelector(".cardWrapper");
     cardWrapper.appendChild(divEl);
@@ -36,10 +47,13 @@ const createMyList = (title, imgUrl, popularity, id, date) => {
     const imgEl = document.createElement("img");
     const pPopEl = document.createElement("p");
     const pDateEl = document.createElement("p");
+    const removeEL = document.createElement("span");
 
     divEl.classList.add("card");
     pPopEl.classList.add("popularity");
     pDateEl.classList.add("date");
+    removeEL.classList.add("material-icons");
+    removeEL.classList.add("remove");
 
     imgEl.setAttribute("src", `https://image.tmdb.org/t/p/original/${imgUrl}`);
     imgEl.setAttribute("alt", "img poster");
@@ -48,8 +62,9 @@ const createMyList = (title, imgUrl, popularity, id, date) => {
     h3El.textContent = title;
     pPopEl.textContent = popularity;
     pDateEl.textContent = date;
+    removeEL.textContent = "close";
 
-    divEl.append(imgEl, h3El, pPopEl, pDateEl);
+    divEl.append(imgEl, removeEL, h3El, pPopEl, pDateEl);
 
     const cardWrapperList = document.querySelector(".cardWrapperList");
     cardWrapperList.appendChild(divEl);
@@ -75,14 +90,21 @@ getMovies(url).then(data => {
             movie.release_date
         );
     })
-    cards = document.querySelectorAll(".card");
+    cards = document.querySelectorAll(".plus");
     console.log(cards);
-    cards.forEach(element => {
+    cards.forEach((element,key) => {
         element.addEventListener("click", () => {
             myList.push(movieArray.find(item =>
                 item.id == element.id
             ));
             console.log(myList);
+
+            element.classList.add("hide");
+
+            const stars = document.querySelectorAll(".star");
+            stars[key].classList.add("show");
+            // stars.forEach(star => );
+
             const cardWrapperList = document.querySelector(".cardWrapperList");
             cardWrapperList.textContent = "";
             myList.map(movie => {
